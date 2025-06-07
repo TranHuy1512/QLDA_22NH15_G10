5e1a628d8a23db91421cc24bbc04cc1951e9868f
Et3rnal <120799346+votienkhoa@users.noreply.github.com>
2025-06-07 14:18:55 +0700

    config vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,25 +10,6 @@ export default defineConfig({
     strictPort: true,
     watch: {
       usePolling: true
-    },
-    proxy: {
-      '^/api/.*': {
-        target: import.meta.env.VITE_API_URL || 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response:', proxyRes.statusCode, req.url);
-          });
-        }
-      }
     }
   }
 })
